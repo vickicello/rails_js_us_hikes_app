a22f390e4d632626aca2921f0e7576b5dc400e29
vickicello <vicki.cello@gmail.com>
2018-11-05 17:59:37 -0800

    started writing associations

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -1,2 +1,6 @@
 class Hike < ApplicationRecord
+  belongs_to :user
+  belongs_to :state
+  has_many :comments
+  has_many :users, through: :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :hikes
+  has_many :comments
+  has_many :commented_hikes, through: :hikes
 end
