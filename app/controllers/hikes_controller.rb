0759db6fd9d55d3dea2a46926e09b83e4da0edb5
vickicello <vicki.cello@gmail.com>
2018-11-07 20:09:50 -0500

    added hike views and stubbed out hike controller

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,2 +1,35 @@
 class HikesController < ApplicationController
+  def index
+    @hikes = Hike.all
+  end
+
+  def show
+    @hike = Hike.find(params[:id])
+  end
+
+  def new
+    @hike = Hike.new
+  end
+
+  def create
+    @hike = Hike.create(hike_params)
+    redirect_to hike_path(@hike)
+    # should we have validations here? if @hike.save, etc
+  end
+
+  def edit
+    @hike = Hike.find(params[:id])
+  end
+
+  def update
+    @hike = Hike.find(params[:id])
+    @hike.update(hike_params)
+    redirect_to hike_path(@hike)
+  end
+
+  private
+
+  def hike_params
+    params.require(:hike).permit(:name, :state_id, :description, :user_id)
+  end
 end
