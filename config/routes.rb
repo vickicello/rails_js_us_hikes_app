45c26eed45a9b3c0760764a2a8d27666f85ee916
vickicello <vicki.cello@gmail.com>
2018-11-12 16:36:46 -0500

    setting up nested attributes and views

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   delete '/signout', to: 'sessions#destroy'
   post '/sessions/signup', to: 'sessions#create'
   get '/auth/facebook/callback' => 'sessions#create'
+  get 'hikes/with_comments', to: 'hikes#with_comments'
+  
+  resources :hikes do
+    resources :comments
+  end
   
   resources :users
   resources :hikes
