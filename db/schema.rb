1f6d9220d20dffb2ef0f68aa13cb554c9a404ee9
vickicello <vicki.cello@gmail.com>
2018-11-12 15:57:05 -0500

    added image column to users table for fb omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_015445) do
+ActiveRecord::Schema.define(version: 2018_11_12_205547) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2018_11_06_015445) do
     t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
