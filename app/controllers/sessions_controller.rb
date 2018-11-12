899a38a882fd57d343561e2510293ed6d3f022b2
vickicello <vicki.cello@gmail.com>
2018-11-12 15:40:49 -0500

    set up omniauth for fb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,26 @@ class SessionsController < ApplicationController
     @user = User.new
   end
 
-  def create
-    @user = User.find_by(username: params[:user][:username])
-    if @user && @user.authenticate(params[:user][:username])
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
-    else
-      redirect_to '/'
+  # omniauth tutorial says I need to do name instead of username?
+  def create 
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.username = auth['info']['username']
+      u.email = auth['info']['email']
+      u.image = auth['info']['image']
     end
+
+    session[:user_id] = @user.id
+    render '/'
   end
 
   def destroy
     session.destroy
     redirect_to root_path
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/signin', to: 'sessions#new'
   delete '/signout', to: 'sessions#destroy'
   post '/sessions/signup', to: 'sessions#create'
+  get '/auth/facebook/callback' => 'sessions#create'
   
   resources :users
   resources :hikes
