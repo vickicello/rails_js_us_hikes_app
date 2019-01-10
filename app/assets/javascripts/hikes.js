a8b47f914c314dfc78f534f3782eff9161859869
vickicello <vicki.cello@gmail.com>
2019-01-09 19:31:25 -0500

    add ajax method incorporating prototype

--- app/assets/javascripts/hikes.js
+++ app/assets/javascripts/hikes.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 	// alert("users.js loaded!");
-
 class Hike {
 	constructor(hikeData){
 		this.name = hikeData.name
@@ -10,23 +9,22 @@ class Hike {
 		this.id = hikeData.id
 		this.userId = hikeData.user_id
 	}
+
+	// static method 
 }
 
 //HTML prototype for formatting and listing user's hikes:
 Hike.prototype.formatHike = function() {
-  return `<li><h3><a href="/hikes/${this.id}", class="show_user_hikes">${this.name}</a></h3>
-    <p>State: ${this.state}</p>
-    <p>Description: ${this.description}</p></li>`
+  return `<li><h3><a href="/hikes/${this.id}", class="show_hike_details">${this.name}</a></h3></li>`
 }
 
 Hike.prototype.hikeInfoTemplate = function() {
   return `<h3>${this.name}</h3>
-    <p>${this.hike.name}</p>
     <p>State: ${this.state}</p>
     <p>Description: ${this.description}</p>`
 }
 
-// request via HTML
+// request via HTML - keep!
 //  $('a.show_user_hikes').on('click', function(e){
 // 	$.ajax({
 // 		method: "GET",
@@ -40,7 +38,26 @@ Hike.prototype.hikeInfoTemplate = function() {
 // 	e.preventDefault();
 // })
 
-//request via JSON
+//request via JSON = keep !!!
+// $('a.show_user_hikes').on('click', function(e){
+// 	$.ajax({
+//     url: this.href,
+//     type: "GET",
+//     dataType: "json",
+// 	  success: function(data){
+// 			//clear ol html:
+// 	   	var $ol = $("div.list_user_hikes ol");
+//     	$ol.html("") //emptied ol
+// 	   	//iterate over each hike with JSON:
+// 			data.forEach(function(hike){
+// 				$ol.append('<li>' + hike.name + ' - ' + hike.state + ' - ' + hike.description + '</li>');
+// 			});
+// 		}
+// 	});
+// 	e.preventDefault();
+// });
+
+//render using prototype
 $('a.show_user_hikes').on('click', function(e){
 	$.ajax({
     url: this.href,
@@ -48,17 +65,28 @@ $('a.show_user_hikes').on('click', function(e){
     dataType: "json",
 	  success: function(data){
 			//clear ol html:
-	   	var $ol = $("div.list_user_hikes ol");
-    	$ol.html("") //emptied ol
-	   	//iterate over each hike with JSON:
-			data.forEach(function(hike){
-				$ol.append('<li>' + hike.name + ' - ' + hike.state + ' - ' + hike.description + '</li>');
-			});
+	   	const $ol = $("div.list_user_hikes ol");
+			$ol.html("") //emptied ol
+			// debugger;
+			data.forEach((hike, index)=>{
+				let newHike = new Hike(hike)
+				let newHikeHTML = newHike.formatHike()
+				$ol.append(newHikeHTML)
+			})
 		}
 	});
 	e.preventDefault();
 });
 
+$('a.show_hike_details').on('click', function(e){
+	$.ajax({
+    url: this.href,
+    type: "GET",
+    dataType: "json",
+	  success: function(data){
+			//clear ol html:
+	   	const $ol = $("div.list_user_hikes ol");
+			$ol.html("") //emptied ol
 
 //hikes#show
 //use class Hike object to create a custom function that displays comments.   
