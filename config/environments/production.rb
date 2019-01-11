5f5b5af9f955aa2fb876458cf90e18badfd5acae
vickicello <vicki.cello@gmail.com>
2019-01-11 18:40:37 -0500

    debugging css load error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,9 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  
+  # precompile css files
+  Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
